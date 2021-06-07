22b7c51669a623a614e1a43c8b15828f875dabb6
Stackbit Code Editor <projects@stackbit.com>
2021-06-07 00:34:13 +0000

    HeroSection.js: changed

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -15,9 +15,7 @@ export default class HeroSection extends React.Component {
                 <h1 className="hero__title line typing-animation">{_.get(section, 'title', null)}</h1>
                 )}
 
-                           {_.get(section, 'title', null) && (
-                <h1 className="hero__title line typing-animation">{_.get(section, 'title', null)}</h1>
-                )}
+                  
       {_.get(section, 'video', null) && (
 <ReactPlayer  playing={true} width="100%" light={true} muted={true} volume={1} url={_.get(section, 'video', null)} />     
   )}
